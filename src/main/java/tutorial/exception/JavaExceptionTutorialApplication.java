a20f6d3d5aa8f5ee17e58a7b404385f1704e0066
codetalker <foraiz86@gmail.com>
2015-08-09 11:10:27 +0900

    first commit

---
+++ src/main/java/tutorial/exception/JavaExceptionTutorialApplication.java
@@ -0,0 +1,12 @@
+package tutorial.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExceptionTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExceptionTutorialApplication.class, args);
+    }
+}
