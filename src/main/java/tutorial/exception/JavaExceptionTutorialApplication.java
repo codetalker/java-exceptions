7f65301439002d96bf98ccbd106bae573e3634f5
codetalker <foraiz86@gmail.com>
2015-08-09 13:15:28 +0900

    Exception종류

--- src/main/java/tutorial/exception/JavaExceptionTutorialApplication.java
+++ src/main/java/tutorial/exception/JavaExceptionTutorialApplication.java
@@ -1,12 +1,14 @@
 package tutorial.exception;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaExceptionTutorialApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaExceptionTutorialApplication.class, args);
+    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(JavaExceptionTutorialApplication.class, args);
+//        throw new Exception("this is a Exception");
+//        throw new RuntimeException("this is a RuntimeException");
+        throw new Error("this is an Error");
     }
 }
