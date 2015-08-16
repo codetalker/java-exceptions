234bc6d0386ca327f3763fb7abba2c4a1dde2176
codetalker <foraiz86@gmail.com>
2015-08-16 09:25:28 +0900

    Add index.html

--- src/main/java/tutorial/exception/JavaExceptionTutorialApplication.java
+++ src/main/java/tutorial/exception/JavaExceptionTutorialApplication.java
@@ -1,14 +1,13 @@
 package tutorial.exception;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaExceptionTutorialApplication {
 
     public static void main(String[] args) throws Exception {
-//        SpringApplication.run(JavaExceptionTutorialApplication.class, args);
-//        throw new Exception("this is a Exception");
-//        throw new RuntimeException("this is a RuntimeException");
-        throw new Error("this is an Error");
+        SpringApplication.run(JavaExceptionTutorialApplication.class, args);
     }
 }
+
